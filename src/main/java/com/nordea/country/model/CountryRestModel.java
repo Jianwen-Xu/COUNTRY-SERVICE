2208ada50573217af755caad190b087eebdbe8f1
Jianwen Xu <st_simon@163.com>
2021-07-03 23:28:38 +0300

    Fix check style format

--- src/main/java/com/nordea/country/model/CountryRestModel.java
+++ src/main/java/com/nordea/country/model/CountryRestModel.java
@@ -1,58 +1,74 @@
+/*
+ * Copyright 2021-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.nordea.country.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import java.util.List;
-
+/**
+ * @author Jianwen Xu
+ */
 @JsonFilter("countryFilter")
 public class CountryRestModel {
-    private String name;
-    @JsonAlias("alpha2Code")
-    private String country_code;
-    private String capital;
-    private Integer population;
-    @JsonAlias("flag")
-    private String flag_file_url;
+	private String name;
+	@JsonAlias("alpha2Code")
+	private String country_code;
+	private String capital;
+	private Integer population;
+	@JsonAlias("flag")
+	private String flag_file_url;
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getCountry_code() {
-        return country_code;
-    }
+	public String getCountry_code() {
+		return country_code;
+	}
 
-    public void setCountry_code(String country_code) {
-        this.country_code = country_code;
-    }
+	public void setCountry_code(String country_code) {
+		this.country_code = country_code;
+	}
 
-    public String getCapital() {
-        return capital;
-    }
+	public String getCapital() {
+		return capital;
+	}
 
-    public void setCapital(String capital) {
-        this.capital = capital;
-    }
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
 
-    public Integer getPopulation() {
-        return population;
-    }
+	public Integer getPopulation() {
+		return population;
+	}
 
-    public void setPopulation(Integer population) {
-        this.population = population;
-    }
+	public void setPopulation(Integer population) {
+		this.population = population;
+	}
 
-    public String getFlag_file_url() {
-        return flag_file_url;
-    }
+	public String getFlag_file_url() {
+		return flag_file_url;
+	}
 
-    public void setFlag_file_url(String flag_file_url) {
-        this.flag_file_url = flag_file_url;
-    }
+	public void setFlag_file_url(String flag_file_url) {
+		this.flag_file_url = flag_file_url;
+	}
 }
